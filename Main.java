750cc2ff659a6150cf22f129b3eb5cfd73fa2873
Mateusz <100692029+Mateusz0231@users.noreply.github.com>
2022-04-12 11:49:26 +0000

    zadanie 3.1 i 3.2

--- Main.java
+++ Main.java
@@ -1,18 +1,81 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    try {
+    try{
       Service1 s = new Service1();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
-
-      var students = s.getStudents();
+      String Name;
+      String Date;
+      int Age;
+      int wybor;
+      int x=0;
+      while(x<3)
+             {
+      System.out.println("Co chcesz zrobić");
+    System.out.println("1.Dopisanie studenta");
+      System.out.println("2.Lista studentów");
+      System.out.println("3.koniec programu");
+    Scanner to = new Scanner(System.in);
+        wybor=to.nextInt();
+      
+      switch(wybor)
+           
+    
+        {
+            
+        //comit 3_1a
+          case 1:
+            
+            System.out.println("Dopisanie studenta");
+            System.out.println("Podaj imię studenta");
+            Name=to.next();
+            System.out.println("Podaj datę urodzenia studenta");
+            Date=to.next();
+            System.out.println("Podaj wiek studenta");
+            Age=to.nextInt();
+            s.addStudent(new Student(Name,Date, Age));
+            
+            break;
+        
+      //comit 3_1b
+        case 2:
+        
+        System.out.println("Lista studentów");
+            var students = s.getStudents();
       for(Student current : students) {
         System.out.println(current.ToString());
-      }
-    } catch (IOException e) {
+        }
+          case 3:
+            System.out.println("Koniec programu");
+            x=4;
+            break;
+        }
+        }
+        
+      
+
+      
+        
+            } catch (IOException e) {
 
     }
-  }
+    
 }
+}
+        
+  
+
+
+
+
+    
+      
+    
+
+    
+
+    
+
+        
+  
--- Student.java
+++ Student.java
@@ -1,24 +1,27 @@
 public class Student {
 
   private String Name;
+  private String Date;
   private int Age;
-
-  public Student(String name, int age) {
+//comit 3_2
+  public Student(String name, String date, int age) {
     Name = name;
+    Date=date;
     Age = age;
   }
 
   public String GetName() {return Name;}
+  public String GetDate() {return Date;}
   public int GetAge() {return Age;}
 
   public String ToString() {
-    return Name + " " + Integer.toString(Age);
+    return Name + " " + Date +" "+ Integer.toString(Age);
   }
 
   public static Student Parse(String str) {
     String[] data = str.split(" ");
-    if(data.length != 2) 
-      return new Student("Parse Error", -1);
-    return new Student(data[0], Integer.parseInt(data[1]));
+    if(data.length != 3) 
+      return new Student("Parse Error","Parse Error", -1);
+    return new Student(data[0],data[1], Integer.parseInt(data[2]));
   }
 }
